f6c53eead49111b1c4ae087be9cf9ea1d4eecd71
Dorai Sitaram <ds26gte@yahoo.com>
2017-01-28 03:29:42 -0500

    Initial commit.

---
+++ Makefile
@@ -0,0 +1,283 @@
+# NOTE: Needs TWO blank lines here, dunno why
+define \n
+
+
+endef
+ifneq ($(findstring .exe,$(SHELL)),)
+	override SHELL:=$(COMSPEC)$(ComSpec)
+	MKDIR = $(foreach dir,$1,if not exist "$(dir)". (md "$(dir)".)$(\n))
+	RMDIR = $(foreach dir,$1,if exist "$(dir)". (rd /S /Q "$(dir)".)$(\n))
+	RM = if exist "$1". (del $1)
+else
+	MKDIR = mkdir -p $1
+	RMDIR = rm -rf $1
+	RM = rm -f $1
+endif
+
+CM=node_modules/codemirror
+CPOMAIN=build/web/js/cpo-main.jarr
+CPOGZ=build/web/js/cpo-main.jarr.gz.js
+CPOIDEHOOKS=build/web/js/cpo-ide-hooks.jarr
+PHASEA=pyret/build/phaseA/pyret.jarr
+
+.PHONY : post-install
+post-install: compress-pyret
+
+install-link:
+	npm link pyret-lang
+
+.PHONY : selenium-test-local
+selenium-test-local:
+	TEST_LOC="local" node test/test.js test/browser
+
+.PHONY : selenium-test-sauce
+selenium-test-sauce:
+	TEST_LOC="sauce" node test/test.js test/browser/pyret
+
+OUT_HTML := $(patsubst src/web/%.template.html,build/web/views/%.html,$(wildcard src/web/*.template.html))
+
+build/web/views/%.html: src/web/%.template.html
+	node make-template.js $< > $@
+
+COPY_HTML := $(patsubst src/web/%.html,build/web/views/%.html,$(wildcard src/web/*.html))
+
+build/web/views/%.html: src/web/%.html
+	cp $< $@
+
+OUT_CSS := $(patsubst src/web/%.template.css,build/web/%.css,$(wildcard src/web/css/*.template.css))
+
+build/web/css/%.css: src/web/css/%.template.css
+	node make-template.js $< > $@
+
+COPY_CSS := $(patsubst src/web/%.css,build/web/%.css,$(wildcard src/web/css/*.css))
+
+build/web/css/%.css: src/web/css/%.css
+	cp $< $@
+
+COPY_FONTS := $(patsubst src/web/%,build/web/%,$(wildcard src/web/css/fonts/*))
+
+build/web/css/fonts/%: src/web/css/fonts/%
+	cp $< $@
+
+COPY_NEW_CSS := $(patsubst src/web/%.css,build/web/%.css,$(wildcard src/web/neweditor/css/*.css))
+
+build/web/neweditor/css/%.css: src/web/neweditor/css/%.css
+	cp $< $@
+
+COPY_NEW_JS := $(patsubst src/web/%.js,build/web/%.js,$(wildcard src/web/neweditor/js/*.js))
+
+build/web/neweditor/js/%.js: src/web/neweditor/js/%.js
+	cp $< $@
+
+build/web/css/codemirror.css: $(CM)/lib/codemirror.css
+	cp $< $@
+
+build/web/css/foldgutter.css: $(CM)/addon/fold/foldgutter.css
+	cp $< $@
+
+MISC_CSS = build/web/css/codemirror.css build/web/css/foldgutter.css
+
+COPY_GIF := $(patsubst src/web/img/%.gif,build/web/img/%.gif,$(wildcard src/web/img/*.gif))
+
+build/web/img/%.gif: src/web/img/%.gif
+	cp $< $@
+
+COPY_JS := $(patsubst src/web/js/%.js,build/web/js/%.js,$(wildcard src/web/js/*.js))
+
+build/web/js/%.js: src/web/js/%.js
+	cp $< $@
+
+COPY_BLOCKS_JS := $(patsubst src/web/js/codemirror-blocks-dist/%.js,build/web/js/%.js,$(wildcard src/web/js/codemirror-blocks-dist/*.js))
+
+build/web/js/%.js: src/web/js/codemirror-blocks-dist/%.js
+	cp $< $@
+
+COPY_GOOGLE_JS := $(patsubst src/web/js/google-apis/%.js,build/web/js/google-apis/%.js,$(wildcard src/web/js/google-apis/*.js))
+
+build/web/js/google-apis/%.js: src/web/js/google-apis/%.js
+	cp $< $@
+
+build/web/js/d3.js: node_modules/d3/d3.min.js
+	cp $< $@
+
+build/web/js/d3-tip.js: node_modules/d3-tip/index.js
+	cp $< $@
+
+build/web/js/beforePyret.js: src/web/js/beforePyret.js
+	(cd src/web/js; `npm bin`/webpack beforePyret.js j.js)
+	cp src/web/js/j.js $@
+
+build/web/js/q.js: node_modules/q/q.js
+	cp $< $@
+
+build/web/js/s-expression-lib.js: node_modules/s-expression/index.js
+	cp $< $@
+
+build/web/js/colorspaces.js: node_modules/colorspaces/colorspaces.js
+	cp $< $@
+
+build/web/js/es6-shim.js: node_modules/es6-shim/es6-shim.min.js
+	cp $< $@
+
+build/web/js/seedrandom.js: node_modules/seedrandom/seedrandom.js
+	cp $< $@
+
+build/web/js/url.js: node_modules/url.js/url.js
+	cp $< $@
+
+build/web/js/require.js: node_modules/requirejs/require.js
+	cp $< $@
+
+build/web/js/codemirror.js: $(CM)/lib/codemirror.js
+	cp $< $@
+
+build/web/js/mark-selection.js: $(CM)/addon/selection/mark-selection.js
+	cp $< $@
+
+build/web/js/runmode.js: $(CM)/addon/runmode/runmode.js
+	cp $< $@
+
+build/web/js/pyret-fold.js: src/web/js/codemirror/pyret-fold.js
+	cp $< $@
+
+build/web/js/matchkw.js: src/web/js/codemirror/matchkw.js
+	cp $< $@
+
+build/web/js/foldcode.js: $(CM)/addon/fold/foldcode.js
+	cp $< $@
+
+build/web/js/foldgutter.js: $(CM)/addon/fold/foldgutter.js
+	cp $< $@
+
+build/web/js/pyret-mode.js: src/web/js/codemirror/pyret-mode.js
+	cp $< $@
+
+MISC_JS = build/web/js/q.js build/web/js/url.js build/web/js/require.js \
+          build/web/js/codemirror.js \
+          build/web/js/mark-selection.js \
+          build/web/js/pyret-mode.js build/web/js/s-expression-lib.js \
+          build/web/js/seedrandom.js \
+          build/web/js/pyret-fold.js \
+          build/web/js/matchkw.js \
+          build/web/js/foldcode.js \
+          build/web/js/foldgutter.js \
+          build/web/js/colorspaces.js \
+          build/web/js/es6-shim.js \
+          build/web/js/runmode.js \
+          build/web/js/d3.js \
+          build/web/js/d3-tip.js
+
+MISC_IMG = build/web/img/pyret-icon.png build/web/img/pyret-logo.png build/web/img/pyret-spin.gif build/web/img/up-arrow.png build/web/img/down-arrow.png build/web/img/BigLogo.png build/web/img/spyret-break.png build/web/img/spyret-run.png build/web/img/spyret-save.png build/web/img/spyret-share.png build/web/img/spyret-undo.png build/web/img/spyret-redo.png build/web/img/spyret-blank.gif build/web/img/spyret-interactions.png build/web/img/spyret-fadeIcon.png build/web/img/spyret-fadeWords.png build/web/img/spyret-treadmill-run.gif build/web/img/googledrive.png
+
+build/web/img/%: node_modules/pyret-lang/img/%
+	cp $< $@
+
+COPY_ARR := $(patsubst ./pyret/src/arr/trove/%.arr,build/web/arr/%.arr,$(wildcard ./pyret/src/arr/trove/*.arr))
+COPY_ARR :=
+
+# build/web/arr/%: pyret/src/arr/trove/%
+# 	cp $< $@
+
+WEB = build/web
+WEBV = build/web/views
+WEBJS = build/web/js
+WEBJSGOOG = build/web/js/google-apis
+WEBCSS = build/web/css
+WEBFONTS = $(WEBCSS)/fonts
+WEBIMG = build/web/img
+WEBARR = build/web/arr
+WEBCOLL = build/web/collections
+NEWCSS = build/web/neweditor/css
+NEWJS = build/web/neweditor/js
+
+$(WEBV):
+	@$(call MKDIR,$(WEBV))
+
+$(WEB):
+	@$(call MKDIR,$(WEB))
+
+$(WEBJS):
+	@$(call MKDIR,$(WEBJS))
+
+$(WEBJSGOOG):
+	@$(call MKDIR,$(WEBJSGOOG))
+
+$(WEBCSS):
+	@$(call MKDIR,$(WEBCSS))
+
+$(WEBFONTS):
+	@$(call MKDIR,$(WEBFONTS))
+
+$(WEBIMG):
+	@$(call MKDIR,$(WEBIMG))
+
+$(WEBARR):
+	@$(call MKDIR,$(WEBARR))
+
+$(WEBCOLL):
+	@$(call MKDIR,$(WEBCOLL))
+	cp -pr src/web/collections/* $@
+
+$(NEWCSS):
+	@$(call MKDIR,$(NEWCSS))
+
+$(NEWJS):
+	@$(call MKDIR,$(NEWJS))
+
+web-local: $(WEB) $(WEBV) $(WEBJS) $(WEBJSGOOG) $(WEBCSS) $(WEBFONTS) $(WEBIMG) $(WEBARR) $(WEBCOLL) $(NEWCSS) $(NEWJS) $(OUT_HTML) $(COPY_HTML) $(OUT_CSS) $(COPY_CSS) $(COPY_FONTS) $(COPY_JS) $(COPY_BLOCKS_JS) $(COPY_ARR) $(COPY_GIF) $(MISC_JS) $(MISC_CSS) $(MISC_IMG) $(COPY_NEW_CSS) $(COPY_NEW_JS) $(COPY_GOOGLE_JS) $(CPOMAIN) $(CPOGZ) $(CPOIDEHOOKS)
+
+web: $(WEB) $(WEBV) $(WEBJS) $(WEBJSGOOG) $(WEBCSS) $(WEBFONTS) $(WEBIMG) $(WEBARR) $(WEBCOLL) $(NEWCSS) $(NEWJS) $(OUT_HTML) $(COPY_HTML) $(OUT_CSS) $(COPY_CSS) $(COPY_FONTS) $(COPY_JS) $(COPY_BLOCKS_JS) $(COPY_ARR) $(COPY_GIF) $(MISC_JS) $(MISC_CSS) $(MISC_IMG) $(COPY_NEW_CSS) $(COPY_NEW_JS) $(COPY_GOOGLE_JS)
+
+link-pyret:
+	ln -s node_modules/pyret-lang pyret;
+	cd node_modules/pyret-lang && $(MAKE) phaseA-deps && cd ../../;
+
+deploy-cpo-main: link-pyret $(CPOMAIN) $(CPOIDEHOOKS) $(CPOGZ)
+
+TROVE_JS := src/web/js/trove/*.js
+TROVE_ARR := src/web/arr/trove/*.arr
+
+$(PHASEA): libpyret ;
+
+.PHONY: libpyret
+libpyret:
+	$(MAKE) phaseA -C pyret/
+
+$(CPOMAIN): $(TROVE_JS) $(TROVE_ARR) $(WEBJS) src/web/js/*.js src/web/arr/*.arr cpo-standalone.js cpo-config.json src/web/arr/cpo-main.arr $(PHASEA)
+	mkdir -p compiled/;
+	cp pyret/build/phaseA/compiled/*.js ./compiled/
+	node pyret/build/phaseA/pyret.jarr \
+    --builtin-js-dir src/web/js/trove/ \
+    --builtin-js-dir pyret/src/js/trove/ \
+    -allow-builtin-overrides \
+    --builtin-arr-dir src/web/arr/trove/ \
+    --builtin-arr-dir pyret/src/arr/trove/ \
+    --require-config cpo-config.json \
+    --build-runnable src/web/arr/cpo-main.arr \
+    --standalone-file cpo-standalone.js \
+    --compiled-dir ./compiled \
+    --outfile $(CPOMAIN) -no-check-mode
+
+# NOTE(joe): Need to do .gz.js because Firefox doesn't like gzipped JS having a
+# non-.js extension.
+$(CPOGZ): $(CPOMAIN)
+	gzip -c -f $(CPOMAIN) > $(CPOGZ)
+
+$(CPOIDEHOOKS): $(TROVE_JS) $(WEBJS) src/web/js/*.js src/web/arr/*.arr cpo-standalone.js cpo-config.json src/web/arr/cpo-ide-hooks.arr $(PHASEA)
+	mkdir -p compiled/;
+	cp pyret/build/phaseA/compiled/*.js ./compiled/
+	node pyret/build/phaseA/pyret.jarr \
+    --builtin-js-dir src/web/js/trove/ \
+    --builtin-js-dir pyret/src/js/trove/ \
+    -allow-builtin-overrides \
+    --builtin-arr-dir src/web/arr/trove/ \
+    --builtin-arr-dir pyret/src/arr/trove/ \
+    --require-config cpo-config.json \
+    --build-runnable src/web/arr/cpo-ide-hooks.arr \
+    --standalone-file cpo-standalone.js \
+    --compiled-dir ./compiled \
+    --outfile $(CPOIDEHOOKS) -no-check-mode
+
+clean:
+	rm -rf build/
+	rm -rf compiled/
---
+++ src/web/js/beforePyret.js
@@ -0,0 +1,436 @@
+/* global $ jQuery CPO CodeMirror storageAPI Q createProgramCollectionAPI makeShareAPI */
+
+//var shareAPI = makeShareAPI(process.env.CURRENT_PYRET_RELEASE);
+var shareAPI = makeShareAPI(env_CURRENT_PYRET_RELEASE);
+
+var url = require('url.js');
+
+const LOG = true;
+window.ct_log = function(/* varargs */) {
+  if (window.console && LOG) {
+    console.log.apply(console, arguments);
+  }
+};
+
+window.ct_error = function(/* varargs */) {
+  if (window.console && LOG) {
+    console.error.apply(console, arguments);
+  }
+};
+var initialParams = url.parse(document.location.href);
+var params = url.parse("/?" + initialParams["hash"]);
+window.highlightMode = "mcmh"; // what is this for?
+window.clearFlash = function() {
+  $(".notificationArea").empty();
+}
+window.stickError = function(message, more) {
+  clearFlash();
+  var err = $("<div>").addClass("error").text(message);
+  if(more) {
+    err.attr("title", more);
+  }
+  err.tooltip();
+  $(".notificationArea").prepend(err);
+};
+window.flashError = function(message) {
+  clearFlash();
+  var err = $("<div>").addClass("error").text(message);
+  $(".notificationArea").prepend(err);
+  err.fadeOut(7000);
+};
+window.flashMessage = function(message) {
+  clearFlash();
+  var msg = $("<div>").addClass("active").text(message);
+  $(".notificationArea").prepend(msg);
+  msg.fadeOut(7000);
+};
+window.stickMessage = function(message) {
+  clearFlash();
+  var err = $("<div>").addClass("active").text(message);
+  $(".notificationArea").prepend(err);
+};
+window.mkWarningUpper = function(){return $("<div class='warning-upper'>");}
+window.mkWarningLower = function(){return $("<div class='warning-lower'>");}
+
+$(window).bind("beforeunload", function() {
+  return "Because this page can load slowly, and you may have outstanding changes, we ask that you confirm before leaving the editor in case closing was an accident.";
+});
+
+var Documents = function() {
+  
+  function Documents() {
+    this.documents = new Map();
+  }
+  
+  Documents.prototype.has = function (name) {
+    return this.documents.has(name);
+  };
+
+  Documents.prototype.get = function (name) {
+    return this.documents.get(name);
+  };
+
+  Documents.prototype.set = function (name, doc) {
+    if(logger.isDetailed)
+      logger.log("doc.set", {name: name, value: doc.getValue()});
+    return this.documents.set(name, doc);
+  };
+  
+  Documents.prototype.delete = function (name) {
+    if(logger.isDetailed)
+      logger.log("doc.del", {name: name});
+    return this.documents.delete(name);
+  };
+
+  Documents.prototype.forEach = function (f) {
+    return this.documents.forEach(f);
+  };
+
+  return Documents;
+}();
+
+window.CPO = {
+  save: function() {},
+  autoSave: function() {},
+  documents : new Documents()
+};
+$(function() {
+  function merge(obj, extension) {
+    var newobj = {};
+    Object.keys(obj).forEach(function(k) {
+      newobj[k] = obj[k];
+    });
+    Object.keys(extension).forEach(function(k) {
+      newobj[k] = extension[k];
+    });
+    return newobj;
+  }
+  var animationDiv = null;
+  function closeAnimationIfOpen() {
+    if(animationDiv) {
+      animationDiv.empty();
+      animationDiv.dialog("destroy");
+      animationDiv = null;
+    }
+  }
+  CPO.makeEditor = function(container, options) {
+    var initial = "";
+    if (options.hasOwnProperty("initial")) {
+      initial = options.initial;
+    }
+
+    var textarea = jQuery("<textarea>");
+    textarea.val(initial);
+    container.append(textarea);
+
+    var runFun = function (code, replOptions) {
+      options.run(code, {cm: CM}, replOptions);
+    };
+
+    var useLineNumbers = !options.simpleEditor;
+    var useFolding = !options.simpleEditor;
+
+    var gutters = !options.simpleEditor ?
+      ["CodeMirror-linenumbers", "CodeMirror-foldgutter"] :
+      [];
+
+    function reindentAllLines(cm) {
+      var last = cm.lineCount();
+      cm.operation(function() {
+        for (var i = 0; i < last; ++i) cm.indentLine(i);
+      });
+    }
+
+    var cmOptions = {
+      extraKeys: {
+        "Shift-Enter": function(cm) { runFun(cm.getValue()); },
+        "Shift-Ctrl-Enter": function(cm) { runFun(cm.getValue()); },
+        "Tab": "indentAuto",
+        "Ctrl-I": reindentAllLines
+      },
+      indentUnit: 2,
+      tabSize: 2,
+      viewportMargin: Infinity,
+      lineNumbers: useLineNumbers,
+      matchKeywords: true,
+      matchBrackets: true,
+      styleSelectedText: true,
+      foldGutter: useFolding,
+      gutters: gutters,
+      lineWrapping: true,
+      logging: true
+    };
+
+    cmOptions = merge(cmOptions, options.cmOptions || {});
+
+    var CM = CodeMirror.fromTextArea(textarea[0], cmOptions);
+
+    var CMblocks;
+
+    if (typeof CodeMirrorBlocks === 'undefined') {
+      console.log('CodeMirrorBlocks not found');
+      CMblocks = undefined;
+    } else {
+      CMblocks = new CodeMirrorBlocks(CM,
+        'wescheme',
+        {
+          willInsertNode: function(sourceNodeText, sourceNode, destination) {
+            var line = CM.editor.getLine(destination.line);
+            if (destination.ch > 0 && line[destination.ch - 1].match(/[\w\d]/)) {
+              // previous character is a letter or number, so prefix a space
+              sourceNodeText = ' ' + sourceNodeText;
+            }
+
+            if (destination.ch < line.length && line[destination.ch].match(/[\w\d]/)) {
+              // next character is a letter or a number, so append a space
+              sourceNodeText += ' ';
+            }
+            return sourceNodeText;
+          }
+        });
+      CM.blocksEditor = CMblocks;
+      CM.changeMode = function(mode) {
+        if (mode === "false") {
+          mode = false;
+        } else {
+          CMblocks.ast = null;
+        }
+        CMblocks.setBlockMode(mode);
+      }
+    }
+
+    if (useLineNumbers) {
+      CM.display.wrapper.appendChild(mkWarningUpper()[0]);
+      CM.display.wrapper.appendChild(mkWarningLower()[0]);
+    }
+
+    return {
+      cm: CM,
+      refresh: function() { CM.refresh(); },
+      run: function() {
+        runFun(CM.getValue());
+      },
+      focus: function() { CM.focus(); }
+    };
+  };
+  CPO.RUN_CODE = function() {
+
+  };
+
+  storageAPI.then(function(api) {
+    api.collection.then(function() {
+      $(".loginOnly").show();
+      $(".logoutOnly").hide();
+      api.api.getCollectionLink().then(function(link) {
+        $("#drive-view a").attr("href", link);
+      });
+    });
+    api.collection.fail(function() {
+      $(".loginOnly").hide();
+      $(".logoutOnly").show();
+    });
+  });
+
+  storageAPI = storageAPI.then(function(api) { return api.api; });
+  $("#connectButton").click(function() {
+    $("#connectButton").text("Connecting...");
+    $("#connectButton").attr("disabled", "disabled");
+    storageAPI = createProgramCollectionAPI("code.pyret.org", false);
+    storageAPI.then(function(api) {
+      api.collection.then(function() {
+        $(".loginOnly").show();
+        $(".logoutOnly").hide();
+        api.api.getCollectionLink().then(function(link) {
+          $("#drive-view a").attr("href", link);
+        });
+        if(params["get"] && params["get"]["program"]) {
+          var toLoad = api.api.getFileById(params["get"]["program"]);
+          console.log("Logged in and has program to load: ", toLoad);
+          loadProgram(toLoad);
+          programToSave = toLoad;
+        } else {
+          programToSave = Q.fcall(function() { return null; });
+        }
+      });
+      api.collection.fail(function() {
+        $("#connectButton").text("Connect to Google Drive");
+        $("#connectButton").attr("disabled", false);
+      });
+    });
+    storageAPI = storageAPI.then(function(api) { return api.api; });
+  });
+
+  var copyOnSave = false;
+
+  var initialProgram = storageAPI.then(function(api) {
+    var programLoad = null;
+    if(params["get"] && params["get"]["program"]) {
+      programLoad = api.getFileById(params["get"]["program"]);
+      programLoad.then(function(p) { showShareContainer(p); });
+    }
+    if(params["get"] && params["get"]["share"]) {
+      programLoad = api.getSharedFileById(params["get"]["share"]);
+      $("#saveButton").text("Save a Copy");
+      copyOnSave = true;
+    }
+    if(programLoad) {
+      programLoad.fail(function(err) {
+        console.error(err);
+        window.stickError("The program failed to load.");
+      });
+      return programLoad;
+    } else {
+      return null;
+    }
+  });
+
+  function setTitle(progName) {
+    document.title = progName + " - code.pyret.org";
+  }
+  CPO.setTitle = setTitle;
+
+  $("#download a").click(function() {
+    var downloadElt = $("#download a");
+    var contents = CPO.editor.cm.getValue();
+    var downloadBlob = window.URL.createObjectURL(new Blob([contents], {type: 'text/plain'}));
+    var filename = $("#program-name").val();
+    if(!filename) { filename = 'untitled_program.arr'; }
+    if(filename.indexOf(".arr") !== (filename.length - 4)) {
+      filename += ".arr";
+    }
+    downloadElt.attr({
+      download: filename,
+      href: downloadBlob
+    });
+    $("#download").append(downloadElt);
+  });
+
+  function loadProgram(p) {
+    return p.then(function(p) {
+      if(p !== null) {
+        $("#program-name").val(p.getName());
+        setTitle(p.getName());
+        return p.getContents();
+      }
+    });
+  }
+
+  var programLoaded = loadProgram(initialProgram);
+
+  var programToSave = initialProgram;
+
+  function showShareContainer(p) {
+    $("#shareContainer").empty();
+    $("#shareContainer").append(shareAPI.makeShareLink(p));
+  }
+
+  function nameOrUntitled() {
+    return $("#program-name").val() || "Untitled";
+  }
+  function autoSave() {
+    programToSave.then(function(p) {
+      if(p !== null && !copyOnSave) { save(); }
+    });
+  }
+  CPO.autoSave = autoSave;
+  CPO.showShareContainer = showShareContainer;
+  CPO.loadProgram = loadProgram;
+
+  function save() {
+    window.stickMessage("Saving...");
+    var savedProgram = programToSave.then(function(p) {
+      if(p !== null && !copyOnSave) {
+        if(p.getName() !== $("#program-name").val()) {
+          programToSave = p.rename(nameOrUntitled()).then(function(newP) {
+            return newP;
+          });
+        }
+        return programToSave
+        .then(function(p) {
+          showShareContainer(p);
+          return p.save(CPO.editor.cm.getValue(), false);
+        })
+        .then(function(p) {
+          $("#program-name").val(p.getName());
+          $("#saveButton").text("Save");
+          history.pushState(null, null, "#program=" + p.getUniqueId());
+          window.location.hash = "#program=" + p.getUniqueId();
+          window.flashMessage("Program saved as " + p.getName());
+          setTitle(p.getName());
+          return p;
+        });
+      }
+      else {
+        var programName = $("#program-name").val() || "Untitled";
+        $("#program-name").val(programName);
+        programToSave = storageAPI
+          .then(function(api) { return api.createFile(programName); });
+        copyOnSave = false;
+        return save();
+      }
+    });
+    savedProgram.fail(function(err) {
+      window.stickError("Unable to save", "Your internet connection may be down, or something else might be wrong with this site or saving to Google.  You should back up any changes to this program somewhere else.  You can try saving again to see if the problem was temporary, as well.");
+      console.error(err);
+    });
+  }
+  CPO.save = save;
+  $("#runButton").click(CPO.autoSave);
+  $("#saveButton").click(save);
+  shareAPI.makeHoverMenu($("#menu"), $("#menuContents"), false, function(){});
+
+  var codeContainer = $("<div>").addClass("replMain");
+  $("#main").prepend(codeContainer);
+
+  CPO.editor = CPO.makeEditor(codeContainer, {
+    runButton: $("#runButton"),
+    simpleEditor: false,
+    run: CPO.RUN_CODE,
+    initialGas: 100
+  });
+  CPO.editor.cm.setOption("readOnly", "nocursor");
+  
+  programLoaded.then(function(c) {
+    CPO.documents.set("definitions://", CPO.editor.cm.getDoc());
+    
+    // NOTE(joe): Clearing history to address https://github.com/brownplt/pyret-lang/issues/386,
+    // in which undo can revert the program back to empty
+    CPO.editor.cm.clearHistory();
+    CPO.editor.cm.setValue(c);
+  });
+
+  programLoaded.fail(function() {
+    CPO.documents.set("definitions://", CPO.editor.cm.getDoc());
+  });
+
+  var pyretLoad = document.createElement('script');
+  /*
+  console.log('process.env is', JSON.stringify(process.env));
+  console.log('process.env.GOOGLE_CLIENT_ID is', process.env.GOOGLE_CLIENT_ID);
+  console.log('process.env.REDISCLOUD_URL is', process.env.REDISCLOUD_URL);
+  console.log('process.env.BASE_URL is', process.env.BASE_URL);
+  console.log('process.env.SESSION_SECRET is', process.env.SESSION_SECRET);
+  console.log('process.env.CURRENT_PYRET_RELEASE is', process.env.CURRENT_PYRET_RELEASE);
+  console.log('process.env.PYRET is', process.env.PYRET);
+  console.log('process.env.PYRET_RELEASE_BASE is', process.env.PYRET_RELEASE_BASE);
+  console.log('clientId is', clientId);
+  */
+  //console.log(process.env.PYRET);
+  //pyretLoad.src = process.env.PYRET;
+  console.log('env_PYRET is', env_PYRET);
+  pyretLoad.src = env_PYRET;
+  pyretLoad.type = "text/javascript";
+  document.body.appendChild(pyretLoad);
+  $(pyretLoad).on("error", function() {
+    $("#loader").hide();
+    $("#runPart").hide();
+    $("#breakButton").hide();
+    window.stickError("Pyret failed to load; check your connection or try refreshing the page.  If this happens repeatedly, please report it as a bug.");
+  });
+
+  programLoaded.fin(function() {
+    CPO.editor.focus();
+    CPO.editor.cm.setOption("readOnly", false);
+  });
+
+});
